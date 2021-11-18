4ac4ac7f352bca52b87651184ac51a725499c850
Muhammad Ibtehaj Khan <ibtehaj.official@gmail.com>
2021-11-18 13:43:17 +0500

    Completed CreateVendorScreen

--- screens/vendor_module/CreateVendorScreen.js
+++ screens/vendor_module/CreateVendorScreen.js
@@ -1,15 +1,46 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { ScrollView, Text } from 'react-native';
+import { ScrollView, View } from 'react-native';
+import ActionBox from '../../components/ActionBox';
 
 import GlobalStyles from '../../styles/GlobalStyles';
+import VendorForm from '../../components/InputForm/VendorForm';
 
 const CreateVendorScreen = ({ navigation }) => {
+	const data = {
+		contact_name: '',
+		contact_phone: '',
+		contact_email: '',
+		business_name: '',
+		business_address: '',
+		business_map: '',
+	};
+
+	const [isSubmitPressed, setSubmitPressed] = useState(false);
+
+	const onSubmit = (value) => {
+		if (value) {
+			// Submit Data Here
+			navigation.goBack();
+		} else {
+			setSubmitPressed(false);
+		}
+	};
+
 	return (
 		<ScrollView contentContainerStyle={GlobalStyles.container}>
 			<StatusBar style='dark' />
 
-			<Text>This is CreateVendorScreen</Text>
+			<VendorForm data={data} verify={isSubmitPressed} onSubmit={onSubmit} />
+
+			<View style={{ marginTop: 8 }}>
+				<ActionBox
+					type='primary'
+					title='Add Vendor'
+					action={() => setSubmitPressed(true)}
+				/>
+				<ActionBox title='Cancel' action={() => navigation.goBack()} />
+			</View>
 		</ScrollView>
 	);
 };
